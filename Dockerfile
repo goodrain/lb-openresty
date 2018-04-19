ffa1517a7dd2d2b8a020ffd711579e5eccf0e0d9
jiajun <kxdmmr@163.com>
2018-04-19 17:13:11 +0800

    [REV] Optimize start check for persistence

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ ENV OPENRESTY_HOME=/usr/local/openresty \
 
 WORKDIR $OPENRESTY_HOME
 
-COPY /conf $OPENRESTY_HOME/nginx/conf
-COPY /lua $OPENRESTY_HOME/nginx/lua
+COPY /conf $OPENRESTY_HOME/nginx/conf.default
+COPY /lua $OPENRESTY_HOME/nginx/lua.default
 COPY /bootstrap.sh /bootstrap.sh
 
 CMD /bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-cd $OPENRESTY_HOME/nginx/conf/balances
-mkdir -p dynamic_certs dynamic_http_servers dynamic_stream_servers dynamic_upstreams
+cd $OPENRESTY_HOME/nginx
 
-$OPENRESTY_HOME/nginx/sbin/nginx -g "daemon off;"
+mkdir -p conf lua
+
+[[ `ls lua|wc -l` == 0 ]] && /bin/cp -rf $OPENRESTY_HOME/nginx/lua.default/* lua/
+
+[[ `ls conf|wc -l` == 0 ]] && {
+  /bin/cp -rf $OPENRESTY_HOME/nginx/conf.default/* conf/
+  cd $OPENRESTY_HOME/nginx/conf/balances
+  mkdir -p dynamic_certs dynamic_http_servers dynamic_stream_servers dynamic_upstreams
+}
+
+exec $OPENRESTY_HOME/nginx/sbin/nginx -g "daemon off;"
